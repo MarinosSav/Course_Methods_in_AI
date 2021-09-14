efcdf2e21103aa8d945468b2165a39e6ec5c2138
MarinosSav <32109442+MarinosSav@users.noreply.github.com>
2021-09-14 14:06:27 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+
+records = []
+training_set = []
+test_set = []
+percent = 0.85
+
 for line in open('dialog_acts.dat', 'r'):
     item = line.rstrip()
     action = item.split(' ', 1)[0]
     utterance = item.replace(action, "")
-    print(item, utterance)
+    records.append((action, utterance))
 
+for i in range(len(records)):
+    if i < percent * len(records):
+        training_set.append(records[i])
+    else:
+        test_set.append(records[i])
+
+#for l in range(len(test_set)):
+#    print(l, "action:", test_set[l][0], "utterance:", test_set[l][1])
