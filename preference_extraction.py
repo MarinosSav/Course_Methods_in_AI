b675bffc05a3c89f134704c2cce34697d04e4470
MarinosSav <32109442+MarinosSav@users.noreply.github.com>
2021-10-09 05:27:56 +0200

    Added comments

--- preference_extraction.py
+++ preference_extraction.py
@@ -1,14 +1,16 @@
-# preference extraction
 import re
 import nltk
 import random
 
+# Regex patterns for each option
 FOOD_PATTERN = r"(\w+)(\sfood|\srestaurant)"
 AREA_PATTERN = r"in the (\w+)"
 PRICE_PATTERN = r"(\w+)(\spriced|\srestaurant)"
-MAX_ALLOWED_DISTANCE = 3
+MAX_ALLOWED_DISTANCE = 3  # maximum levenshtein distance
+
 
 def find_nearest_option(word, options):
+    """Returns the closest distance Levenshtein word (word) from a set of options (options)."""
     closest_options = []
     current_best_distance = 999
     for option in options:
@@ -22,8 +24,10 @@ def find_nearest_option(word, options):
         return random.choice(closest_options)
     return None
 
-def get_preferences(utterance, food_options, area_options, price_options):
 
+def get_preferences(utterance, food_options, area_options, price_options):
+    """Extracts the preferences of the user for food type (food_match), area (area_match) and price range (price_match)
+    from a utterance (utterance). Takes as input the option set for each."""
     food_match, area_match, price_match = check_exact_matches(utterance, food_options, area_options, price_options)
     if food_match:
         food_match = food_match[0]
@@ -41,7 +45,10 @@ def get_preferences(utterance, food_options, area_options, price_options):
 
     return food_match, area_match, price_match
 
+
 def check_pattern_matches(utterance, pattern, options):
+    """Checks if a given word pattern (pattern) is detected within a utterance (utterance), before attempting to find
+    the closes possible word. If no word is found None is returned."""
     match = re.search(pattern, utterance)
     if match:
         word = match[1]
@@ -50,7 +57,10 @@ def check_pattern_matches(utterance, pattern, options):
         return find_nearest_option(word, options)
     return None
 
+
 def check_exact_matches(utterance, food_options, area_options, price_options):
+    """Checks utterance (utterance) for exact matches from the options sets for food type (food_options), area
+    (area_options) and price range (price_options)."""
     food_match = [ele for ele in food_options if(ele in utterance)]
     area_match = [ele for ele in area_options if(ele in utterance)]
     price_match = [ele for ele in price_options if(ele in utterance)]
