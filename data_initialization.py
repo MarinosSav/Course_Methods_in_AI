f6f03e1cf8b5093a0e366a1f2dd4ec2f4603ea08
MarinosSav <32109442+MarinosSav@users.noreply.github.com>
2021-10-09 05:27:32 +0200

    Added comments

--- data_initialization.py
+++ data_initialization.py
@@ -1,11 +1,14 @@
 from csv import DictReader
 from reasoning import add_extra_restaurant_properties
 
-RESTAURANT_INFO_FILENAME = 'restaurant_info_new.csv'
+# stored files
+RESTAURANT_INFO_FILENAME = 'restaurant_info.csv'
 DIALOG_DATASET_FILENAME = 'dialog_acts.dat'
 
+
 def fetch_sample_dialogs_data():
-    """Reads input file and splits into utterances and dialog actions"""
+    """Reads dialog input file and splits data into two lists, one for utterances and one for dialog actions; returns
+    these lists."""
     utterances = []
     actions = []
     with open(DIALOG_DATASET_FILENAME) as fp:
@@ -17,20 +20,25 @@ def fetch_sample_dialogs_data():
             actions.append(action.lower())
     return utterances, actions
 
+
 def fetch_restaurant_info():
+    """Reads the file containing restaurant information and writes it as a list."""
     with open(RESTAURANT_INFO_FILENAME, 'r') as read_obj:
         dict_reader = DictReader(read_obj)
         restaurant_list = list(dict_reader)
     return restaurant_list
 
+
 def initialize_restaurant_info():
+    """Loads the restaurant information add additional fields."""
     restaurant_info = fetch_restaurant_info()
     updated_restaurant_info = add_extra_restaurant_properties(restaurant_info)
-    print(updated_restaurant_info)
     return updated_restaurant_info
 
 
 def get_category_options(restaurants_info):
+    """Creates sets for each potential option a user can choose for each restaurant and returns them; one for food
+    (food_options), one for area (area_options) and one for price range options (pricerange_options)."""
     food_options = set([d['food'] for d in restaurants_info if 'food' in d])
     area_options = set([d['area'] for d in restaurants_info if 'area' in d])
     pricerange_options = set([d['pricerange'] for d in restaurants_info if 'pricerange' in d])
